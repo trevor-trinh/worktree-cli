175f2d2bc3b85cc4ab3202035b9e21df0ea72ef1
John Lindquist <johnlindquist@gmail.com>
2025-03-28 10:30:16 -0600

    feat: Add aliases for list and remove commands
    Added 'ls' as an alias for 'list'.
    
    Added 'rm' as an alias for 'remove'.

--- src/index.ts
+++ src/index.ts
@@ -28,11 +28,13 @@ program
 
 program
     .command("list")
+    .alias("ls")
     .description("List all existing worktrees for this repository.")
     .action(listWorktreesHandler);
 
 program
     .command("remove")
+    .alias("rm")
     .argument("[pathOrBranch]", "Path of the worktree or branch to remove.")
     .option("-f, --force", "Force removal of worktree and deletion of the folder", false)
     .description("Remove a specified worktree. Cleans up the .git/worktrees references.")
